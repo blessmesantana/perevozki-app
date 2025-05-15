0d3e1f36053144c4a1908917dfaf780b3a7bab2c
blessmesantana <167603008+blessmesantana@users.noreply.github.com>
2025-05-15 11:26:39 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,19 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+// firebase.js
 
-// Конфигурация Firebase
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js ';
+import { getDatabase } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js ';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyB5n7xxHc45VINsoVKkTcXUuUm4TsMk8bg",
-    authDomain: "perevozki-88c5b.firebaseapp.com",
-    projectId: "perevozki-88c5b",
-    storageBucket: "perevozki-88c5b.firebasestorage.app",
-    messagingSenderId: "962120908044",
-    appId: "1:962120908044:web:bd5a49937daf33d825dd29"
+  apiKey: "AIzaSyB5n7xxHc45VINsoVKkTcXUuUm4TsMk8bg",
+  authDomain: "perevozki-88c5b.firebaseapp.com",
+  databaseURL: "https://perevozki-88c5b-default-rtdb.europe-west1.firebasedatabase.app ",
+  projectId: "perevozki-88c5b",
+  storageBucket: "perevozki-88c5b.appspot.com",
+  messagingSenderId: "962120908044",
+  appId: "1:962120908044:web:bd5a49937daf33d825dd29"
 };
 
-// Инициализация Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const database = getDatabase(app);
 
-export { db };
+export { database };
