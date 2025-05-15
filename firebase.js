f7f56cd7667869f3c2e42cbd6ea69f429fe1ce5f
blessmesantana <167603008+blessmesantana@users.noreply.github.com>
2025-05-15 09:00:29 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,23 +1,18 @@
-// firebase.js
-
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-// Твоя конфигурация Firebase (берётся из Firebase Console)
+// Конфигурация Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyB5n7xxHc45VINsoVKkTcXUuUm4TsMk8bg",
-  authDomain: "perevozki-88c5b.firebaseapp.com",
-  projectId: "perevozki-88c5b",
-  storageBucket: "perevozki-88c5b.firebasestorage.app",
-  messagingSenderId: "962120908044",
-  appId: "1:962120908044:web:bd5a49937daf33d825dd29"
+    apiKey: "AIzaSyB5n7xxHc45VINsoVKkTcXUuUm4TsMk8bg",
+    authDomain: "perevozki-88c5b.firebaseapp.com",
+    projectId: "perevozki-88c5b",
+    storageBucket: "perevozki-88c5b.firebasestorage.app",
+    messagingSenderId: "962120908044",
+    appId: "1:962120908044:web:bd5a49937daf33d825dd29"
 };
 
-// Инициализируем Firebase
+// Инициализация Firebase
 const app = initializeApp(firebaseConfig);
-
-// Инициализируем Firestore
 const db = getFirestore(app);
 
-// Экспортируем db для использования в main.js
 export { db };
