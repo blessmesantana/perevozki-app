e18d8c90d331615b9ebc1399e8693d8c8c9e0c26
blessmesantana <167603008+blessmesantana@users.noreply.github.com>
2025-05-16 06:56:26 +0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,12 +1,10 @@
-// firebase.js
-
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js ';
-import { getDatabase } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js ';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
+import { getDatabase } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js';
 
 const firebaseConfig = {
   apiKey: "AIzaSyB5n7xxHc45VINsoVKkTcXUuUm4TsMk8bg",
   authDomain: "perevozki-88c5b.firebaseapp.com",
-  databaseURL: "https://perevozki-88c5b-default-rtdb.europe-west1.firebasedatabase.app ",
+  databaseURL: "https://perevozki-88c5b-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "perevozki-88c5b",
   storageBucket: "perevozki-88c5b.appspot.com",
   messagingSenderId: "962120908044",
